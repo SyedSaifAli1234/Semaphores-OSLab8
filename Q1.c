4c7599f44dea8d67ff0edf4e7c12617be9f03f00
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-04-14 16:56:26 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -5,15 +5,52 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+char* buffer;
+sem_t *sem1;
+sem_t *sem2;
 
-// Create the shared memory segment or use an already created shared memory segment (shmget())
+void *Producer(void *){
+	printf("Hello producer\n");
+}
 
-// Attach the process to the already created shared memory segment (shmat())
-
-// Detach the process from the already attached shared memory segment (shmdt())
-
-// Control operations on the shared memory segment (shmctl())
+void *Consumer(void *){
+	printf("Hello consumer\n");
+}
 
 
-//int shmget(key_t key, size_t size, int shmflg)
-int shmget(121212, 1024, )
+int main(){
+	
+	pthread_t id1, id2;
+
+	int id_sem=shmget(990, 1024, 0666 | IPC_CREAT);
+	if(id_sem == -1){
+		printf("Error!\n");
+		return 1;
+	}
+
+	buffer = (char*) shmat(id_sem, NULL, 0);
+	if (buffer == -1){
+		printf("Shared memory not created\n");
+	}
+	printf("Shared memory created\n");
+
+
+	if (pthread_create(&id1, NULL, & Producer, NULL) < 0) {
+    	printf("Thread not created\n");
+  	}
+	if (pthread_create(&id2, NULL, & Consumer, NULL) < 0) {
+    	printf("Thread not created\n");
+  	}
+
+  	if (pthread_join(id1, NULL) < 0) {
+    	printf("Thread not created\n");
+  	}
+  	if (pthread_join(id2, NULL) < 0) {
+   	 	printf("Thread not created\n");
+  	}
+
+  	shmdt(id_sem);
+  	shmctl(id_sem, IPC_RMID, 0);
+  	printf("Shared memory destroyed\n");
+
+}
