be207c1782df2af849d019b147b32754dad275b6
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-04-14 17:24:58 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -23,17 +23,36 @@ int main(){
 	
 	pthread_t id1, id2;
 
-	int id_sem=shmget(990, 1024, 0666 | IPC_CREAT);
-	if(id_sem == -1){
+	int buff_shm=shmget(990, 1024, 0666 | IPC_CREAT);									//shared memory created
+	if(buff_shm == -1){
 		printf("Error!\n");
 		return 1;
 	}
 
-	buffer = (char*) shmat(id_sem, NULL, 0);
-	if (buffer == -1){
+	buffer = (char*) shmat(buff_shm, NULL, 0);										//shared memory attached
+	if (buffer == -1){																
 		printf("Shared memory not created\n");
 	}
-	printf("Shared memory created\n");
+	printf("Shared memory created for buffer\n");
+
+
+
+
+	int sem_shm1=shmget(991, 1024, 0666 | IPC_CREAT);									//shared memory created
+	if(sem_shm1 == -1){
+		printf("Error!\n");
+		return 1;
+	}
+
+	sem1 = (sem_t*) shmat(sem_shm1, NULL, 0);										//shared memory attached
+	if (sem1 == -1){																
+		printf("Shared memory not created\n");
+	}
+	printf("Shared memory created for Sem1\n");
+
+
+    
+
 
 
 	if (pthread_create(&id1, NULL, & Producer, NULL) < 0) {
@@ -50,8 +69,11 @@ int main(){
    	 	printf("Thread not created\n");
   	}
 
-  	//shmdt(id_sem);
-  	//shmctl(id_sem, IPC_RMID, 0);
+  	shmdt(buff_shm);
+  	shmctl(buff_shm, IPC_RMID, 0);
+  	shmdt(sem_shm1);
+  	shmctl(sem_shm1, IPC_RMID, 0);
+
   	printf("Shared memory was destroyed\n");
 
 }
